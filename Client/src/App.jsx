48074af4824fb604fde1a14aaeb796989ed77797
Muhammad Nakib Uddin <fliptechofficial@gmail.com>
2026-05-28 11:45:02 +0000

    Add react-toastify for success notifications and update App component

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,24 +1,33 @@
 import axios from "axios";
+import { useState } from "react";
+import { ToastContainer, toast } from 'react-toastify';
 
 const App = () => {
+  const [loading, setLoading] = useState(false);
   const handleSubmit = e => {
     e.preventDefault();
 
+    setLoading(true);
     const image = e.target.image.files[0];
 
     const formData = new FormData();
     formData.append("image", image);
 
     axios.post("http://127.0.0.1:5000/upload", formData)
-      .then(res => console.log(res));
-    console.log(image);
+      .then(res => {
+        if(res.status === 200){
+            toast.success("Your image has been uploaded successfully!");
+        }
+        setLoading(false);
+      });
   }
   return (
     <div className="p-10 min-h-screen bg-black">
       <form onSubmit={handleSubmit} className="flex flex-col items-start gap-5">
         <input type="file" accept="image/*" name="image" className="p-10 bg-gray-400 rounded-xl border border-blue-300" />
-        <button type="submit" className="text-xl px-3 py-2 bg-gray-400 rounded-xl">Submit</button>
+        <button type="submit" className="text-xl px-3 py-2 bg-gray-400 rounded-xl">{loading ? "Loading..." : "Submit"}</button>
       </form>
+      <ToastContainer/>
     </div>
   );
 };
